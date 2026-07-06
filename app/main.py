cb8eba7bcd9ea14965050e425801c6ffcec60e5e
gokul <gokul@DESKTOP-3EB0QH9>
2026-07-06 16:09:14 +0530

    Add root endpoint

--- app/main.py
+++ app/main.py
@@ -4,10 +4,17 @@ from app.routes.chat import router as chat_router
 from app.routes.image import router as image_router
 from app.routes.image_generate import router as image_generate_router
 
+
 app = FastAPI(
     title="NEXORA AI Backend",
     version="1.0.0"
 )
+@app.get("/")
+def home():
+    return {
+        "status": "running",
+        "message": "NEXORA AI Backend"
+    }
 
 # Chat AI
 app.include_router(chat_router)
