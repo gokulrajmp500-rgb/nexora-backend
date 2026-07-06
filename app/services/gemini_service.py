0ebdbc3c50a8df380047ea490264678ee1789229
gokul <gokul@DESKTOP-3EB0QH9>
2026-07-06 17:45:34 +0530

    Fix Gemini model

--- app/services/gemini_service.py
+++ app/services/gemini_service.py
@@ -3,7 +3,7 @@ import google.generativeai as genai
 
 genai.configure(api_key=os.getenv("GEMINI_API_KEY"))
 
-model = genai.GenerativeModel("gemini-1.5-flash")
+model = genai.GenerativeModel("gemini-2.5-flash")
 
 
 # ✅ CHAT FUNCTION
