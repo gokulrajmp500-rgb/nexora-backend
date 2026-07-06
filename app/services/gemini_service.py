3b7054a914b89bcb718f39db4a14567797784341
gokul <gokul@DESKTOP-3EB0QH9>
2026-07-06 18:41:27 +0530

    Fixed image analyze

--- app/services/gemini_service.py
+++ app/services/gemini_service.py
@@ -1,12 +1,19 @@
 import os
+import io
+
+from PIL import Image
 import google.generativeai as genai
 
+# Configure Gemini
 genai.configure(api_key=os.getenv("GEMINI_API_KEY"))
 
-print(os.getenv("GEMINI_API_KEY"))
+# Gemini Model
 model = genai.GenerativeModel("gemini-2.5-flash")
 
 
+# ==========================
+# CHAT
+# ==========================
 def ask_gemini(prompt: str):
     try:
         response = model.generate_content(prompt)
@@ -15,12 +22,20 @@ def ask_gemini(prompt: str):
         return str(e)
 
 
+# ==========================
+# IMAGE ANALYZE
+# ==========================
 def analyze_image(image_bytes: bytes, prompt: str):
     try:
+        # Convert bytes into PIL Image
+        image = Image.open(io.BytesIO(image_bytes))
+
         response = model.generate_content([
             prompt,
-            image_bytes,
+            image
         ])
+
         return response.text
+
     except Exception as e:
         return str(e)
